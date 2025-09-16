8716db0f2463741d1785533efab7a91500fdc510
christopher-nafrere <idunfretmanager@gmail.com>
2025-09-16 15:56:25 +0200

    init prisma and create user structure

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserService } from './user/user.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 import Joi from 'joi';
 
 @Module({
@@ -20,8 +23,9 @@ import Joi from 'joi';
                 DATABASE_URL: Joi.string().required(),
             }),
         }),
+        PrismaModule,
     ],
     controllers: [AppController, AuthController],
-    providers: [AppService],
+    providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
